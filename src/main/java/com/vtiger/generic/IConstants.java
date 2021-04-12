dc809ae8943deb74fac146af2b050ecfad608cee
DELL <DELL@DESKTOP-U5A1O4K>
2021-04-12 16:50:48 +0530

    First commit

---
+++ src/main/java/com/vtiger/generic/IConstants.java
@@ -0,0 +1,13 @@
+package com.vtiger.generic;
+
+public interface IConstants 
+{
+
+	public String propfilepath="../vtiger12/commonData.properties";
+	String excelpath="../vtiger12/TestData.xlsx";
+	int implicitwaittime=10;
+	int explicitwaittime=20;
+
+	
+	
+}
